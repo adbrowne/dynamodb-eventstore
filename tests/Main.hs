e234f2ddbc344dfea9a3b9feba9ffaf13489ae78
Andrew Browne <brownie@brownie.com.au>
2015-02-21 20:54:45 +1100

    Removed unused library

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,7 +3,6 @@ module Main (main) where
 
 import           Test.Hspec
 import           Test.Hspec.Wai
-import           Test.Hspec.Wai.JSON
 import           Network.Wai.Test (SResponse)
 import           Network.HTTP.Types (methodPost)
 import           Network.Wai (Application)
