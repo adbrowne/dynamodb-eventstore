048c306f413273ee0bea1843d0f2f75d5bf56b96
Andrew Browne <brownie@brownie.com.au>
2015-03-02 22:41:16 +1100

    Removed old Webserver tests

--- tests/tastytests.hs
+++ tests/tastytests.hs
@@ -16,10 +16,9 @@ import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.Hspec
 import qualified Data.ByteString.Lazy as BL
-import           Control.Applicative (pure)
 import qualified Webserver as W
 import qualified Web.Scotty as S
-
+import           WebserverSpec
 type FakeDB = Map EventKey (EventType, BS.ByteString)
 
 runTest :: MonadState FakeDB m => EventStoreCmdM a -> m a
@@ -36,44 +35,6 @@ testKey = EventKey ((StreamId "Browne"), 0)
 sampleWrite :: EventStoreCmdM EventWriteResult
 sampleWrite = do
   writeEvent' testKey "FooCreatedEvent" BS.empty
-
-addEventPost :: [H.Header] -> Session SResponse
-addEventPost headers =
-  request $ defaultRequest {
-               pathInfo = ["streams","streamId"],
-               requestMethod = methodPost,
-               requestHeaders = headers,
-               requestBody = pure "" }
-
-postEventSpec :: Spec
-postEventSpec = do
-  let requestWithExpectedVersion = addEventPost [("ES-ExpectedVersion", "1")]
-  let requestWithoutExpectedVersion = addEventPost []
-  let requestWithoutBadExpectedVersion = addEventPost [("ES-ExepectedVersion", "NotAnInt")]
-
-  describe "Parse Int64 header" $ do
-    it "responds with 200" $
-      waiCase requestWithExpectedVersion $ assertStatus 200
-
-    it "responds with body" $
-      waiCase requestWithExpectedVersion $ assertBody "StreamId:streamId expectedVersion:1"
-
-  describe "POST /streams/streamId without ExepectedVersion" $ do
-    it "responds with 400" $
-      waiCase requestWithoutExpectedVersion $ assertStatus 400
-
-  describe "POST /streams/streamId without ExepectedVersion greater than Int64.max" $ do
-    it "responds with 400" $
-       (addEventPost [("ES-ExpectedVersion", "9223372036854775808")]) `waiCase` (assertStatus 400)
-
-  describe "POST /streams/streamId with non-integer ExpectedVersion" $ do
-    it "responds with 400" $
-      requestWithoutBadExpectedVersion `waiCase` (assertStatus 400)
-  where
-    app = S.scottyApp W.app
-    waiCase request assertion = do
-      app' <- app
-      flip runSession app' $ assertion =<< request
 
 main :: IO ()
 main = do
