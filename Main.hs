e8c021c56a990cb56bc46d6ff338f50e5b46c63c
Andrew Browne <brownie@brownie.com.au>
2015-04-06 17:06:34 +0800

    Ensure we can perform IO in Scotty actions

--- Main.hs
+++ Main.hs
@@ -3,8 +3,11 @@ module Main where
 import           Data.Text.Lazy (Text, pack)
 import           Web.Scotty
 import           Webserver      (app)
+import           Control.Monad.IO.Class (liftIO)
 
 showEvent :: Show a => a -> ActionM ()
-showEvent a = (html . pack . show) a
+showEvent a = do
+  liftIO $ print "SomeIo"
+  (html . pack . show) a
 
 main = scotty 3000 (app showEvent)
