a3b5cd919dfe780a3cd2a26ced7a792b6e9ee3d0
Andrew Browne <brownie@brownie.com.au>
2015-06-13 16:17:57 +1000

    Added very simple http client sample

---
+++ client/Main.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Network.Wreq
+import Control.Lens
+import           System.Random
+import qualified Data.Text               as T
+import           Data.Aeson
+
+main :: IO ()
+main = do
+  streamIdNum :: Int <- getStdRandom (randomR (1,9999999999))
+  let streamId = "foo" ++ (show streamIdNum) -- random stream
+  let streamUri = "http://localhost:3000/streams/" ++ streamId
+  let opts = defaults
+                 & header "ES-ExpectedVersion" .~ ["0"]
+                 & header "ES-EventType" .~ ["EventType0"]
+  _ <- postWith opts streamUri (toJSON ("content" :: T.Text))
+  r <- get streamUri
+  putStrLn $ show $ r ^. responseBody
