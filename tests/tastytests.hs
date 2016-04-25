5dd9cfe719cf9ca2339f0010dc6aa6d20057e1d2
Andrew Browne <abrowne@seek.com.au>
2016-04-25 17:14:41 +1000

    Run dynamo cmd tests against test interpreter

--- tests/tastytests.hs
+++ tests/tastytests.hs
@@ -3,14 +3,22 @@
 module Main where
 
 import qualified DynamoCmdAmazonkaTests
+import           DynamoDbEventStore.EventStoreCommands
 import           DynamoDbEventStore.GlobalFeedWriterSpec as GlobalFeedWriterSpec
 import qualified DynamoDbEventStore.AmazonkaInterpreter as Ai
+import qualified DynamoDbEventStore.DynamoCmdInterpreter as TestInterpreter
 
 import           Test.Tasty
 import           Test.Tasty.Hspec
 import qualified WebserverInternalSpec
 import           WebserverSpec
 
+
+testInterpreter :: DynamoCmdM a -> IO a
+testInterpreter program = do 
+  let result = TestInterpreter.evalProgram "Test Program" program TestInterpreter.emptyTestState
+  case result of (Left b)  -> error $ show b
+                 (Right a) -> return a
 
 main :: IO ()
 main = do
@@ -20,6 +28,7 @@ main = do
   defaultMain $
     testGroup "Tests"
       [ testGroup "DynamoCmd Tests against Dynamo - Amazonka" (DynamoCmdAmazonkaTests.tests Ai.evalProgram),
+        testGroup "DynamoCmd Tests against Test Interpreter" (DynamoCmdAmazonkaTests.tests testInterpreter),
         testGroup "Global Feed Writer" GlobalFeedWriterSpec.tests,
         postEventSpec',
         getStreamSpec',
