35219e95c0bb675f99647d4fafe90c7d4290db21
Andrew Browne <abrowne@seek.com.au>
2016-05-23 02:01:13 +1000

    Add missing Feed.hs

---
+++ src/DynamoDbEventStore/Feed.hs
@@ -0,0 +1,157 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards     #-}
+module DynamoDbEventStore.Feed (jsonFeed,jsonEntry,Feed(..),recordedEventToFeedEntry,recordedEventsToFeed) where
+
+import           BasicPrelude
+import           Data.Aeson
+import qualified Data.Attoparsec.ByteString as APBS
+import qualified Data.Vector               as V
+import qualified Data.Text as T
+import           Data.Time.Clock
+import           Data.Time.Format
+import           DynamoDbEventStore.EventStoreCommands
+
+data Feed
+ = Feed
+      { feedId           :: Text
+      , feedTitle        :: Text
+      , feedUpdated      :: UTCTime
+      , feedSelfUrl      :: Text
+      , feedStreamId     :: Text
+      , feedAuthor       :: Author
+      , feedLinks        :: [Link]
+      , feedEntries      :: [Entry]
+      }
+     deriving (Show)
+
+data Author
+ = Author
+     { authorName  :: Text
+     }
+     deriving (Show)
+
+data Entry
+ = Entry
+      { entryId           :: Text
+      , entryTitle        :: Text
+      , entryUpdated      :: UTCTime
+      , entryContent      :: EntryContent
+      , entryLinks        :: [Link]
+      , entrySummary      :: Text
+      }
+     deriving (Show)
+
+data EntryContent
+ = EntryContent 
+     { entryContentEventStreamId :: Text
+     , entryContentEventNumber   :: Int64
+     , entryContentEventType     :: Text
+     , entryContentData                 :: Maybe Value
+     }
+     deriving (Show)
+
+data Link
+ = Link
+      { linkHref     :: Text
+      , linkRel      :: Text
+      }
+     deriving (Show)
+
+recordedEventsToFeed :: Text -> StreamId -> UTCTime -> [RecordedEvent] -> Feed
+recordedEventsToFeed baseUri (StreamId streamId) updated xs = 
+  let
+    selfuri = baseUri <> "/" <> streamId
+  in Feed
+       { feedId = selfuri
+       , feedTitle = "EventStream '" <> streamId <> "'"
+       , feedUpdated = updated
+       , feedSelfUrl = selfuri
+       , feedStreamId = streamId
+       , feedAuthor = Author { authorName = "EventStore" }
+       , feedLinks = []
+       , feedEntries = recordedEventToFeedEntry baseUri <$> xs
+       }
+
+recordedEventToFeedEntry :: Text -> RecordedEvent -> Entry
+recordedEventToFeedEntry baseUri recordedEvent = 
+  let
+    streamId = recordedEventStreamId recordedEvent
+    eventNumber = (show . recordedEventNumber) recordedEvent 
+    eventCreated = recordedEventCreated recordedEvent
+    eventUri = baseUri <> "/" <> streamId <> "/" <> eventNumber
+    title = eventNumber <>  "@" <> streamId
+    updated = eventCreated
+    summary = recordedEventType recordedEvent
+    dataField :: Maybe Value = 
+      if recordedEventIsJson recordedEvent then
+        let 
+          binaryData = recordedEventData recordedEvent
+        in (APBS.maybeResult  (APBS.parse json binaryData))
+      else Nothing
+    content = EntryContent {
+                  entryContentEventStreamId =  recordedEventStreamId recordedEvent
+                , entryContentEventNumber = recordedEventNumber recordedEvent
+                , entryContentEventType = recordedEventType recordedEvent
+                , entryContentData = dataField
+              }
+    links = [
+        Link { linkHref = eventUri, linkRel = "edit" }
+      , Link { linkHref = eventUri, linkRel = "alternatvie" }
+      ]
+  in Entry {
+      entryId = eventUri
+      , entryTitle = title
+      , entryUpdated = updated
+      , entrySummary = summary
+      , entryContent = content
+      , entryLinks = links
+     } 
+
+jsonLink :: Link -> Value
+jsonLink Link {..} =
+  object [ "relation" .= linkRel, "uri" .= linkHref ]
+
+jsonEntryContent :: EntryContent -> Value
+jsonEntryContent EntryContent{..} = 
+  let 
+    addDataField Nothing xs = xs
+    addDataField (Just x) xs = ("data" .= x):xs
+    standardFields = [ 
+        "eventStreamId" .= entryContentEventStreamId
+      , "eventNumber" .= entryContentEventNumber
+      , "eventType" .= entryContentEventType]
+  in object $ addDataField entryContentData standardFields
+
+jsonAuthor :: Author -> Value
+jsonAuthor Author {..} =
+  object [ "name" .= authorName ]
+
+jsonFeed :: Feed -> Value
+jsonFeed Feed {..} = 
+  let 
+    title = "title" .= feedTitle
+    feedid = "id" .= feedId
+    headofstream = "headOfStream" .= True
+    updated = "updated" .= formatJsonTime feedUpdated
+    selfurl = "selfUrl" .= feedSelfUrl
+    streamid = "streamId" .= feedStreamId
+    etag = "etag" .= ( "todo" :: Text)
+    author = "author" .= jsonAuthor feedAuthor
+    links = "links" .= (Array . V.fromList) (jsonLink <$> feedLinks) 
+    entries = "entries" .= (Array . V.fromList) (jsonEntry <$> feedEntries)
+  in object [ title, feedid, updated, streamid, author, headofstream, selfurl, etag, links, entries]
+
+formatJsonTime :: UTCTime -> Text
+formatJsonTime = T.pack . (formatTime defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%SZ")))
+
+jsonEntry :: Entry -> Value
+jsonEntry entry =
+  let
+    entryid = "id" .= entryId entry
+    title = "title" .= entryTitle entry
+    updated = "updated" .= (formatJsonTime $ entryUpdated entry)
+    summary = "summary" .= entrySummary entry
+    content = "content" .= (jsonEntryContent $ entryContent entry)
+    links = "links" .= (Array . V.fromList) (jsonLink <$> entryLinks entry) 
+  in object [entryid, title, summary, content, links, updated]
