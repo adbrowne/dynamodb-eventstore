3c2934e3b466eb61b68c1f2a026f39022d0d89b3
Andrew Browne <brownie@brownie.com.au>
2016-07-10 17:40:42 +1000

    Added test for Json encoding

--- src/DynamoDbEventStore/Webserver.hs
+++ src/DynamoDbEventStore/Webserver.hs
@@ -14,6 +14,7 @@ module DynamoDbEventStore.Webserver(
   , EventStoreActionRunner(..)
   , parseGlobalFeedPosition
   , globalFeedPositionToText
+  , knownJsonKeyOrder
   ) where
 
 import           BasicPrelude
--- tests/DynamoDbEventStore/FeedOutputSpec.hs
+++ tests/DynamoDbEventStore/FeedOutputSpec.hs
@@ -3,12 +3,15 @@
 module DynamoDbEventStore.FeedOutputSpec (tests) where
 
 import           BasicPrelude
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
 import           Data.Maybe                            (fromJust)
 import           Data.Time.Format
 import qualified Data.UUID                             as UUID
 import           DynamoDbEventStore.EventStoreActions
 import           DynamoDbEventStore.EventStoreCommands
 import           DynamoDbEventStore.Feed
+import           DynamoDbEventStore.Webserver
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Text.Blaze.Renderer.String
@@ -69,7 +72,36 @@ streamFeedXmlOutputCheck =
     expectedXml = "<feed xmlns=\"http://www.w3.org/2005/Atom\"><title>Event stream 'MyStream'</title><id>http://localhost:2113/streams/MyStream</id><updated>2016-05-08T12:49:41.000000Z</updated><author><name>EventStore</name></author><link href=\"http://localhost:2113/streams/MyStream\" rel=\"self\" /><link href=\"http://localhost:2113/streams/MyStream/head/forward/1\" rel=\"first\" /><entry><title>2@MyStream</title><id>http://localhost:2113/streams/MyStream/2</id><updated>2016-05-08T12:49:41.000000Z</updated><author><name>EventStore</name></author><summary>Event Type</summary><link href=\"http://localhost:2113/streams/MyStream/2\" rel=\"edit\" /><link href=\"http://localhost:2113/streams/MyStream/2\" rel=\"alternate\" /></entry></feed>"
   in assertEqual "feed is equal to expected xml" expectedXml (renderMarkup $ xmlFeed buildFeed)
 
+encodeJson :: ToJSON a => a -> LByteString
+encodeJson = encodePretty' defConfig {
+  confIndent = 2
+  , confCompare = keyOrder knownJsonKeyOrder }
+
+readEventResultJsonValue :: Text -> RecordedEvent -> Value
+readEventResultJsonValue baseUri recordedEvent =
+  jsonEntry $ recordedEventToFeedEntry baseUri recordedEvent
+
+eventJsonOutputCheck :: Assertion
+eventJsonOutputCheck =
+  let
+    baseUri = "http://localhost:2113"
+    sampleTime = parseTimeOrError True defaultTimeLocale rfc822DateFormat "Sun, 08 May 2016 12:49:41 +0000" -- todo
+    buildFeed = encodeJson $ readEventResultJsonValue baseUri recordedEvent
+    recordedEvent = RecordedEvent{
+            recordedEventStreamId = "MyStream"
+            , recordedEventNumber = 2
+            , recordedEventData = fromString "{ \"a\": 2 }"
+            , recordedEventType = "Event Type"
+            , recordedEventCreated = sampleTime
+            , recordedEventId = EventId (fromJust $ UUID.fromString "1449f441-e249-4381-92b8-a3e2a444c95c")
+            , recordedEventIsJson = True
+                    }
+
+    expectedJson = "{\n  \"title\": \"2@MyStream\",\n  \"id\": \"http://localhost:2113/streams/MyStream/2\",\n  \"updated\": \"2016-05-08T12:49:41.000000Z\",\n  \"author\": {\n    \"name\": \"EventStore\"\n  },\n  \"summary\": \"Event Type\",\n  \"content\": {\n    \"eventStreamId\": \"MyStream\",\n    \"eventNumber\": 2,\n    \"eventType\": \"Event Type\",\n    \"eventId\": \"1449f441-e249-4381-92b8-a3e2a444c95c\",\n    \"data\": {\n      \"a\": 2\n    }\n  },\n  \"links\": [\n    {\n      \"uri\": \"http://localhost:2113/streams/MyStream/2\",\n      \"relation\": \"edit\"\n    },\n    {\n      \"uri\": \"http://localhost:2113/streams/MyStream/2\",\n      \"relation\": \"alternate\"\n    }\n  ]\n}"
+  in assertEqual "output is equal to expected json" expectedJson buildFeed
+
 tests :: [TestTree]
 tests = [
   testCase "Global Feed XML output check" globalFeedXmlOutputCheck
-  , testCase "Stream Feed XML output check" streamFeedXmlOutputCheck]
+  , testCase "Stream Feed XML output check" streamFeedXmlOutputCheck
+  , testCase "Event json output check" eventJsonOutputCheck]
